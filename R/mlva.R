14553bb1494168d67ce2bb5f9b52a8af23da5878
Jerome Ambroise <jerome.ambroise@uclouvain.be>
2019-03-18 11:38:12 +0100

    modification to handle reversecomplement aspects

--- R/mlva.R
+++ R/mlva.R
@@ -1,14 +1,19 @@
-type.mlva <- function(genome,start,end,motif)
+typemlva <- function (genome, start, end, motif)
 {
-  nhit <- numeric()
+  nhit.forward <- numeric()
+  nhit.reverse <- numeric()
 
-  for(i in 1:40)
-  {
-    sequence.to.search <- paste0(start,paste(rep(motif,i),collapse = ''),end)
-    mycount <- vcountPattern(pattern = sequence.to.search,subject = genome,max.mismatch = 0)
-    nhit[i] <- sum(mycount)
+
+  for (i in 1:30) {
+    sequence.to.search <- paste0(start, paste(rep(motif,i), collapse = ""), end)
+    mycount.forward <- vcountPattern(pattern = sequence.to.search,subject = genome, max.mismatch = 1)
+    nhit.forward[i] <- sum(mycount.forward)
+    mycount.reverse <- vcountPattern(pattern = as.character(reverseComplement(DNAString(sequence.to.search))),subject = genome, max.mismatch = 1)
+    nhit.reverse[i] <- sum(mycount.reverse)
   }
 
-  result <- which(nhit==1)
+  nhit <- nhit.forward + nhit.reverse
+
+  result <- which(nhit == 1)
   return(result)
 }
